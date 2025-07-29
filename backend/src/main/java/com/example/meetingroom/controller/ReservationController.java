2a025fa72bc5e579c2ca394ac16b75aae6c60338
Hidenori Mishima <mishima.hidenori@gmail.com>
2025-07-29 17:58:58 +0900

    会議室予約。削除の実装。

--- backend/src/main/java/com/example/meetingroom/controller/ReservationController.java
+++ backend/src/main/java/com/example/meetingroom/controller/ReservationController.java
@@ -5,6 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.meetingroom.dto.ReservationDto;
 import com.example.meetingroom.service.ReservationService;
@@ -26,6 +29,13 @@ public class ReservationController {
         model.addAttribute("reservations", reservationService.getAllReservations());
         model.addAttribute("reservationDto", new ReservationDto());
         return "reservation/index";
+    }
+
+    @PostMapping(value = "/reservation/{id}", params = "_method=delete")
+    public String deleteReservation(@PathVariable Long id, RedirectAttributes redirectAttributes) {
+        reservationService.deleteById(id);
+        redirectAttributes.addFlashAttribute("successMessage", "会議室予約を削除しました。");
+        return "redirect:/reservation";
     }
 
 }
