9f10e947c3ac442a7061aabd6f1c326a350b799f
Hidenori Mishima <mishima.hidenori@gmail.com>
2025-07-30 12:19:13 +0900

    会議室予約。新規登録の実装(仕掛)。

--- backend/src/main/java/com/example/meetingroom/controller/ReservationController.java
+++ backend/src/main/java/com/example/meetingroom/controller/ReservationController.java
@@ -4,7 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -12,6 +15,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.example.meetingroom.dto.ReservationDto;
 import com.example.meetingroom.entity.Reservation;
 import com.example.meetingroom.service.ReservationService;
+import com.example.meetingroom.service.RoomService;
+import com.example.meetingroom.service.UserService;
 
 @Controller
 public class ReservationController {
@@ -19,9 +24,16 @@ public class ReservationController {
     private static final Logger logger = LoggerFactory.getLogger(ReservationController.class);
 
     private final ReservationService reservationService;
+    private final RoomService roomService;
+    private final UserService userService;
 
-    ReservationController(ReservationService reservationService) {
+    ReservationController(
+            ReservationService reservationService,
+            RoomService roomService,
+            UserService userService) {
         this.reservationService = reservationService;
+        this.roomService = roomService;
+        this.userService = userService;
     }
 
     @GetMapping("reservation")
@@ -29,7 +41,42 @@ public class ReservationController {
 
         model.addAttribute("reservations", reservationService.getAllReservations());
         model.addAttribute("reservationDto", new ReservationDto());
+
+        model.addAttribute("rooms", roomService.getAllRooms());
+        model.addAttribute("users", userService.getAllUsers());
+
         return "reservation/index";
+    }
+
+    @PostMapping("reservation")
+    public String createReservation(
+            @Validated @ModelAttribute ReservationDto reservationDto,
+            BindingResult bindingResult,
+            RedirectAttributes redirectAttributes,
+            Model model) {
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("reservations", reservationService.getAllReservations()); // 再表示用
+            model.addAttribute("rooms", roomService.getAllRooms());
+            model.addAttribute("users", userService.getAllUsers());
+
+            return "reservation/index";
+        }
+
+        Reservation newReservation = new Reservation();
+        newReservation.setId(reservationDto.getId());
+        newReservation.setRoom(reservationDto.getRoom());
+        newReservation.setUser(reservationDto.getUser());
+        newReservation.setUseFromDatetime(reservationDto.getUseFromDatetime());
+        newReservation.setUseToDatetime(reservationDto.getUseToDatetime());
+        newReservation.setRemarks(reservationDto.getRemarks());
+
+        reservationService.save(newReservation);
+
+        // トースト表示用に成功メッセージをフラッシュスコープに保存
+        redirectAttributes.addFlashAttribute("successMessage", "予約を登録しました");
+
+        return "redirect:/reservation";
     }
 
     @GetMapping(value = "/reservation/{id}")
